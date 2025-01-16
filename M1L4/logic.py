45ff5a818cc87f206a96a7ba521d5d8c8442b868
Mr-DM <161638656+Mr-DM@users.noreply.github.com>
2025-01-16 16:59:24 +0200

    Update logic.py

--- M1L4/logic.py
+++ M1L4/logic.py
@@ -12,8 +12,19 @@ class Pokemon:
         self.img = self.get_img()
         self.name = self.get_name()
 
+        self.hp = 20
+        self.power = 4 
+
         Pokemon.pokemons[pokemon_trainer] = self
 
+
+    def attack(self, enemy):
+        enemy.hp -= self.power
+        if enemy.hp > 0:
+            return f"Enemy HP: {enemy.hp}"
+        else:
+            return "It lose"
+        
     # Метод для получения картинки покемона через API
     def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -37,11 +48,31 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Name your pokemon: {self.name} Strong: {self.power} Health: {self.hp}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        self.power += 5 
+        result = super().attack(enemy)
+        self.power -= 5
+        return result
+    def info(self):
+        return f"Name your Fighter pokemon: {self.name} Strong: {self.power} Health: {self.hp}"
 
+class Wizard(Pokemon):
+    def attack(self, enemy):
+        self.hp += 5
+        result = super().attack(enemy)
+        self.hp -= 5
+        return result
+    def info(self):
+        return f"Name your Wizard pokemon: {self.name} Strong: {self.power} Health: {self.hp}"
 
+if __name__ == "__main__":
+    Pokemon1 = Pokemon("")
+    Pokemon2 = Pokemon("")
+    print(Pokemon1.attack(Pokemon2))
