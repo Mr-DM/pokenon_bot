7a375415ec004cbada67c21a246ab13994381bee
Mr-DM <161638656+Mr-DM@users.noreply.github.com>
2025-01-30 17:16:31 +0200

    Update main.py

--- M1L4/main.py
+++ M1L4/main.py
@@ -2,21 +2,20 @@ import telebot
 import random
 from config import token
 
-from logic import Pokemon , Fighter, Wizard
+from logic import Pokemon , Fighter
 
 bot = telebot.TeleBot(token) 
 
-random_power = random.randint(1, 3)
+
 
 @bot.message_handler(commands=['go'])
 def go(message):
+    random_power = random.randint(1, 3)
     if message.from_user.username not in Pokemon.pokemons.keys():
         if random_power == 1:
             pokemon = Pokemon(message.from_user.username)
         elif random_power == 2:
             pokemon = Fighter(message.from_user.username)
-        elif random_power == 3:
-            pokemon = Wizard(message.from_user.username)
 
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
@@ -34,5 +33,17 @@ def pokemon_attack(message):
 
         bot.reply_to(message.chat.id, "Command /attack need to replay message ")
 
+@bot.message_handler(commands=["help"])
+def help(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pok.info( ))
+
+@bot.message_handler(commands=["feed"])
+def help(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pok.feed())
+    
 bot.infinity_polling(none_stop=True)
 
